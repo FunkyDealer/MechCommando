7c68c19e25779f8d3cfba587297921fdf7c8a093
Cteps <63469310+Cteps@users.noreply.github.com>
2020-10-21 16:25:13 +0100

    AimSpecialWeapon
    Adicionado o calculo do enimigo mais próximo para disparar depois

---
+++ Mech Commando/Assets/Scripts/Weapons/AntiTankMissile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiTankMissile : MonoBehaviour
+{
+    Vector3 direction;
+    Vector3 position;
+    GameObject Enemy;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Mech Commando/Assets/Scripts/Weapons/EspecialAim.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EspecialAim : MonoBehaviour
+{
+    public GameObject ourGameObj;
+    public Vector3 position;
+    public List<GameObject> Inimigos;
+    public float range;
+    void Start()
+    {
+        position = ourGameObj.transform.position;
+        Inimigos = new List<GameObject>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            if (Inimigos.Contains(other.gameObject) == false)
+            {
+                Inimigos.Add(other.gameObject);
+            }
+        }
+    }
+
+    void Update()
+    {
+        position = ourGameObj.transform.position;
+        GameObject go = null;
+        float distancia = range;
+
+        foreach (GameObject g in Inimigos)
+        {
+            Vector3 p = g.transform.position;
+            float diX = (position.x - p.x) * (position.x - p.x);
+            float diY = (position.y - p.y) * (position.y - p.y);
+            float diZ = (position.z - p.z) * (position.z - p.z);
+
+            float di = Mathf.Sqrt(diX + diY + diZ);
+
+            if (di < distancia)
+            {
+                distancia = di;
+                go = g;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //Disparar Bala
+            Debug.Log("Piu - " + go.transform.position);
+        }
+
+        foreach (GameObject g in Inimigos)
+        {
+            if (g != go)
+            {
+                Inimigos.Remove(g);
+                break;
+            }
+        }
+    }
+}
