4ce87e647d1e072434dcd41d4449b2abf797876c
Cteps <63469310+Cteps@users.noreply.github.com>
2021-01-06 17:02:28 +0000

    Spawner Error
    Tenta Descobrir qual é o null

---
+++ Mech Commando/Assets/Scripts/Spawn/Spawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenuAttribute(menuName = ("Spawn/Entity"))]
+public class Spawn : ScriptableObject
+{
+    [SerializeField]
+    GameObject gameObject;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    public void Create(Vector3 POS)
+    {
+        Instantiate(gameObject,POS,Quaternion.identity);
+    }
+}
+
---
+++ Mech Commando/Assets/Scripts/Spawn/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    Spawn spawn;
+    [SerializeField]
+    int limit;
+    int count;
+    [SerializeField]
+    GameObject pos;
+    [SerializeField]
+    GameObject pos2;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (limit > count)
+        {
+            Debug.Log("Criado");
+            
+            spawn.Create(pos.transform.position);
+            count++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
